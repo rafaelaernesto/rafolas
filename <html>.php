71003a4cf114ced707a2407681c1eb67cffc00e5
rafaelaernesto <159541119+rafaelaernesto@users.noreply.github.com>
2024-06-26 08:18:41 -0300

    Add files via upload

---
+++ <html>.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+   
+</head>
+<body>
+    <form action="tratar.php" method="POST">
+        <input type="text" name="nome">
+        <input type="password" name="senha">
+        <input type="submit">
+    </form>    
+</body>
+</html>
